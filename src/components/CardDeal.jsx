7fdf6156ea24e928bb3ca0d7227e781c697e0c9d
JeanFelixHAKIZIMANA <hakfelix90gmail.com>
2024-02-10 14:23:11 +0200

    first commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,21 @@
+import { card } from '../assets'
+import styles, { layout } from '../style'
+import Button from './Button'
+
+const CardDeal = () => (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a card deal <br className='sm:block hidden' />in few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[480px] mt-5`} >
+        There's only one way under high heaven to get anybody to do anything. Did you ever stop to think of that? Yes, just one way. And that is by making the other person want to do it.
+        </p>
+        <Button styles="mt-10"/>
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt='card' className='w-[100%] h-[100%]'/>
+      </div>
+    </section>
+  )
+
+export default CardDeal
